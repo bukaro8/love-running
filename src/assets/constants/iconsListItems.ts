36c4211a3427f8a7595d28ec88fccaabfad2b473
Victor Ramirez <vicdeveloper83@gmail.com>
2024-12-16 19:12:50 +0000

    footer created with interactive icons list

---
+++ src/assets/constants/iconsListItems.ts
@@ -0,0 +1,7 @@
+// add new icons here
+export const iconsList = [
+	{ name: 'facebook', link: 'https://facebook.com' },
+	{ name: 'twitter', link: 'https://twitter.com' },
+	{ name: 'youtube', link: 'https://youtube.com' },
+	{ name: 'instagram', link: 'https://instagram.com' },
+];
