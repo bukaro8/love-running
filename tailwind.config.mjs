8ae3a2461aada9e36f174a3b8600d11d9abab635
Victor Ramirez <vicdeveloper83@gmail.com>
2024-12-16 14:34:29 +0000

    fonts added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			Oswald: ['Oswald', 'Arial'],
+			Ubuntu: ['Ubuntu', 'sands-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
-}
+};
